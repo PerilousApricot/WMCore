25606b881fcd8b05761864f9d0a765ee952f313b
Andrew Melo <andrew.melo@gmail.com>
2012-07-21 15:08:18 -0500

    Fixing forceUserStorage to create the right LFNs

--- src/python/WMCore/WMSpec/StdSpecs/StdBase.py
+++ src/python/WMCore/WMSpec/StdSpecs/StdBase.py
@@ -477,9 +477,9 @@ class StdBase(object):
             lfnBase(unmergedLFN)
             lfnBase(mergedLFN)
 
-        if forceMerged:
+        if forceMerged and not self.forceUserStorage:
             unmergedLFN = mergedLFN
-        elif forceUnmerged:
+        elif forceUnmerged or self.forceUserStorage:
             mergedLFN = unmergedLFN
 
         cmsswStep = parentTask.getStep(stepName)
